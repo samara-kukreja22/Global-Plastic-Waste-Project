c585d2b4652dac7018038c3741ae83910831c07b
samara-kukreja22 <55211612+samara-kukreja22@users.noreply.github.com>
2021-11-19 11:56:11 -0500

    stuff

--- JSONgenerator.js
+++ JSONgenerator.js
@@ -5,15 +5,16 @@ let totalWasteGeneration = [];
 let macroWasteGen_csv = fs.readFileSync('data/macro-totalWasteGeneration.csv', 'utf8');//storing csv: organizes data into a table format
 //universal text format 8 bits
 //stored in that variable
+let perCapita_csv = fs.readFileSync('data/barChart-PerCapita.csv', 'utf8');
+let inadequateWaste_csv = fs.readFileSync('data/piChart-inadequate.csv', 'utf8');
+let mismanagedWaste_csv = fs.readFileSync('data/piChart-GlobalMismanaged.csv', 'utf8');
 
 let totalWasteGeneration = macroWasteGen_csv.split("\n");//is an array, each row is a different element
 
-totalWasteGeneration.forEach(function(countries) {
+plasticWaste.forEach(function(countries) {
   let country_info = country.split(',');
   let country = {};
   country['entity'] = country_info[1];
-  country['code'] = country_info[2];
-  country['year'] = country_info[3];
   country['pwg'] = country_info[4];
   //much easier to label specific info about each character
   //figure out how to go from csv to JSON
