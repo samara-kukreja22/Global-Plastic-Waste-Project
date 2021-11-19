fc6545bcedfa08b7ed7474d82af1a5a4db4ea87e
samara-kukreja22 <55211612+samara-kukreja22@users.noreply.github.com>
2021-11-19 12:04:58 -0500

    wrong json

--- JSONgenerator.js
+++ JSONgenerator.js
@@ -1,17 +1,18 @@
 const fs = require('fs');//like dice or scorecard
 
-let totalWasteGeneration = [];
+let  plasticData= [];
 
 let macroWasteGen_csv = fs.readFileSync('data/macro-totalWasteGeneration.csv', 'utf8');//storing csv: organizes data into a table format
-//universal text format 8 bits
-//stored in that variable
 let perCapita_csv = fs.readFileSync('data/barChart-PerCapita.csv', 'utf8');
 let inadequateWaste_csv = fs.readFileSync('data/piChart-inadequate.csv', 'utf8');
 let mismanagedWaste_csv = fs.readFileSync('data/piChart-GlobalMismanaged.csv', 'utf8');
 
 let totalWasteGeneration = macroWasteGen_csv.split("\n");//is an array, each row is a different element
+let perCapitaWaste = perCapita_csv.split("\n");
+let inadequateWaste = inadequateWaste_csv.split("\n");
+let mismanagedWaste = mismanagedWaste_csv.split("\n");
 
-plasticWaste.forEach(function(countries) {
+totalWasteGeneration.forEach(function(countries) {
   let country_info = country.split(',');
   let country = {};
   country['entity'] = country_info[1];
@@ -20,4 +21,4 @@ plasticWaste.forEach(function(countries) {
   //figure out how to go from csv to JSON
 
 
-fs.writeFileSync('data/macroPlasticWaste.json', JSON.stringify(totalWasteGeneration), 'utf8');//stringify and save it and then you can also parse it
+fs.writeFileSync('data/plasticData.json', JSON.stringify(plasticData), 'utf8');//stringify and save it and then you can also parse it
