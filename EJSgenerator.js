dcb1a5b34af9b5da668fa082df92fd6400fa4273
samara-kukreja22 <55211612+samara-kukreja22@users.noreply.github.com>
2021-11-22 11:33:01 -0500

    indent

--- EJSgenerator.js
+++ EJSgenerator.js
@@ -5,10 +5,10 @@ let country_info = fs.readFileSync('data/plasticData.json', 'utf8');//read JSON 
 let index_template = fs.readFileSync('views/index.ejs', 'utf8');
 
 for(i=0; i<12; i++){
-let index_html = ejs.render(index_template, {//renders the file
+  let index_html = ejs.render(index_template, {//renders the file
   filename: __dirname + '/views/index.ejs',
   data: JSON.parse(country_info)
-});
+  });
 
-fs.writeFileSync('build/index.html', index_html, 'utf8');//save the file in the build folder
+  fs.writeFileSync('build/index.html', index_html, 'utf8');//save the file in the build folder
 }
