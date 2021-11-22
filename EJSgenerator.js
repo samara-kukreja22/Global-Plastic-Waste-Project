84e4f9351714c3f8a614bbdaa1a13750c9d3205c
samara-kukreja22 <55211612+samara-kukreja22@users.noreply.github.com>
2021-11-22 08:54:12 -0500

    Create EJSgenerator.js

---
+++ EJSgenerator.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const ejs = require('ejs');
+
+let country_info = fs.readFileSync('data/plasticData.json', 'utf8');//read JSON object and sotre it here
+let index_template = fs.readFileSync('views/index.ejs', 'utf8');
+
+let index_html = ejs.render(index_template, {//renders the file
+  filename: __dirname + '/views/index.ejs',
+  data: JSON.parse(country_info)
+});
+
+fs.writeFileSync('build/index.html', index_html, 'utf8');//save the file in the build folder
+//instead of one file make 10 files/11 files
