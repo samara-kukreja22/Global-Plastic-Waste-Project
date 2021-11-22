425422939ed49efd1798c79e6ec142c51cd4bfae
erincorridon <erin.corridon22@trinityschoolnyc.org>
2021-11-22 11:55:19 -0500

    changes

--- EJSgenerator.js
+++ EJSgenerator.js
@@ -4,6 +4,8 @@ const ejs = require('ejs');
 let country_info = fs.readFileSync('data/plasticData.json', 'utf8');//read JSON object and sotre it here
 let index_template = fs.readFileSync('views/index.ejs', 'utf8');
 
+console.log(country_info);
+//for(let i in country_info)
 let index_html = ejs.render(index_template, {//renders the file
   filename: __dirname + '/views/index.ejs',
   data: JSON.parse(country_info)
