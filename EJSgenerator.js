eef9e86927fd59a6de45f9d28dd9dec027e7fd21
samara-kukreja22 <55211612+samara-kukreja22@users.noreply.github.com>
2021-11-22 11:40:28 -0500

    created views

--- EJSgenerator.js
+++ EJSgenerator.js
@@ -4,11 +4,9 @@ const ejs = require('ejs');
 let country_info = fs.readFileSync('data/plasticData.json', 'utf8');//read JSON object and sotre it here
 let index_template = fs.readFileSync('views/index.ejs', 'utf8');
 
-for(i=0; i<12; i++){
-  let index_html = ejs.render(index_template, {//renders the file
+let index_html = ejs.render(index_template, {//renders the file
   filename: __dirname + '/views/index.ejs',
   data: JSON.parse(country_info)
-  });
+});
 
-  fs.writeFileSync('build/index.html', index_html, 'utf8');//save the file in the build folder
-}
+fs.writeFileSync('build/index.html', index_html, 'utf8');//save the file in the build folder
