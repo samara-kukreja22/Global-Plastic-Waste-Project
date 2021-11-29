ac3743994c2be017f8d505c5bde9567bff70eed5
erincorridon <erin.corridon22@trinityschoolnyc.org>
2021-11-29 12:07:35 -0500

    new updates w/ ejs

--- EJSgenerator.js
+++ EJSgenerator.js
@@ -4,8 +4,8 @@ const ejs = require('ejs');
 let country_info = fs.readFileSync('data/plasticData.json', 'utf8');//read JSON object and sotre it here
 let index_template = fs.readFileSync('views/index.ejs', 'utf8');
 
-let parsedObject = JSON.parse(country_info);
-console.log(country_info);
+let data = JSON.parse(country_info);
+//console.log(country_info);
 //console.log(JSON.parse(country_info)[1]);
 
 let index_html = ejs.render(index_template, {//renders the macropage
@@ -15,10 +15,12 @@ let index_html = ejs.render(index_template, {//renders the macropage
 
 fs.writeFileSync('build/index.html', index_html, 'utf8');//save the file in the build folder */ //standard w/out loop
 
-for(let object of parsedObject){
+for(let object of data){
   let microHTML = ejs.render(index_template, {//renders the file
       filename: __dirname + '/views/micro.ejs',
       data: object
     });
+    //console.log(1);
+    //console.log(object);
   fs.writeFileSync('build/'+ object.entity +'.html', microHTML, 'utf8');
 }
