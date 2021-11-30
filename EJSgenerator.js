58f2ec82fdd96c56e4444d50e7435d35857dc227
samara-kukreja22 <55211612+samara-kukreja22@users.noreply.github.com>
2021-11-30 14:54:13 -0500

    Update EJSgenerator.js

--- EJSgenerator.js
+++ EJSgenerator.js
@@ -4,7 +4,7 @@ const ejs = require('ejs');
 let country_info = fs.readFileSync('data/plasticData.json', 'utf8');//read JSON object and sotre it here
 let index_template = fs.readFileSync('views/index.ejs', 'utf8');
 
-let data = JSON.parse(country_info);
+let data = JSON.parse(country_info);//data object
 //console.log(country_info);
 //console.log(JSON.parse(country_info)[1]);
 
@@ -15,7 +15,7 @@ let index_html = ejs.render(index_template, {//renders thbe macropage
 
 fs.writeFileSync('build/index.html', index_html, 'utf8');//save the file in the build folder */ //standard w/out loop
 
-for(let object of data){
+for(let object of data){//going through the values of data
   let microHTML = ejs.render(index_template, {//renders the file
       filename: __dirname + '/views/micro.ejs',
       data: object
