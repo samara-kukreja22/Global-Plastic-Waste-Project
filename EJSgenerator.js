9e842e39043a0ddc1d5e86395c813875cc8ba456
erincorridon <erin.corridon22@trinityschoolnyc.org>
2021-11-30 14:55:41 -0500

    updates

--- EJSgenerator.js
+++ EJSgenerator.js
@@ -7,15 +7,13 @@ let micro_template = fs.readFileSync('views/micro.ejs', 'utf8');
 
 
 let data = JSON.parse(country_info);//data object
-//console.log(country_info);
-//console.log(JSON.parse(country_info)[1]);
 
-let index_html = ejs.render(index_template, {//renders thbe macropage
+let index_html = ejs.render(index_template, {//renders the macropage
     filename: __dirname + '/views/index.ejs',
     data: JSON.parse(country_info)
   });
 
-fs.writeFileSync('build/index.html', index_html, 'utf8');//save the file in the build folder */ //standard w/out loop
+fs.writeFileSync('build/index.html', index_html, 'utf8');//save the file in the build folder
 
 for(let object of data){//going through the values of data
   let microHTML = ejs.render(index_template, {//renders the file
