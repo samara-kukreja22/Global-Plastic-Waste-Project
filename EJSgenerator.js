a53024b24ca1dae52a30fd653fcbb52fac8856f6
erincorridon <erin.corridon22@trinityschoolnyc.org>
2021-11-30 14:54:22 -0500

    fixing micro_template

--- EJSgenerator.js
+++ EJSgenerator.js
@@ -3,6 +3,8 @@ const ejs = require('ejs');
 
 let country_info = fs.readFileSync('data/plasticData.json', 'utf8');//read JSON object and sotre it here
 let index_template = fs.readFileSync('views/index.ejs', 'utf8');
+let micro_template = fs.readFileSync('views/micro.ejs', 'utf8');
+
 
 let data = JSON.parse(country_info);
 //console.log(country_info);
@@ -16,7 +18,7 @@ let index_html = ejs.render(index_template, {//renders thbe macropage
 fs.writeFileSync('build/index.html', index_html, 'utf8');//save the file in the build folder */ //standard w/out loop
 
 for(let object of data){
-  let microHTML = ejs.render(index_template, {//renders the file
+  let microHTML = ejs.render(micro_template, {//renders the file
       filename: __dirname + '/views/micro.ejs',
       data: object
     });
