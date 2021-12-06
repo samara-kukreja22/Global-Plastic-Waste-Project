743e0494b3c4a1e045b0a550b11b0292eb755012
samara-kukreja22 <55211612+samara-kukreja22@users.noreply.github.com>
2021-12-06 11:30:56 -0500

    build working, server.js,

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const fs = require('fs');
+
+const app = express();
+const port = 3000;
+
+app.use(express.static('build'));
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
