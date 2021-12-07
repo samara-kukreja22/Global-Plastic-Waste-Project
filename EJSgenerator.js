b276a13fd203eb9a42ad9c202bb70c8eabc8bb76
erincorridon <erin.corridon22@trinityschoolnyc.org>
2021-12-07 12:55:55 -0500

    making about dynamic (had to add a another piece of data to the ejs.render for micropages)

--- EJSgenerator.js
+++ EJSgenerator.js
@@ -4,6 +4,7 @@ const ejs = require('ejs');
 let country_info = fs.readFileSync('data/plasticData.json', 'utf8');//read JSON string (array of objects) and store it here
 let index_template = fs.readFileSync('views/index.ejs', 'utf8');
 let micro_template = fs.readFileSync('views/micro.ejs', 'utf8');
+let about_template = fs.readFileSync('views/about.ejs', 'utf8');
 
 let data = JSON.parse(country_info);//convert JSON string into a JS object
 
@@ -17,7 +18,15 @@ fs.writeFileSync('build/index.html', index_html, 'utf8');//save the file in the 
 for(let object of data){//going through the values of data
   let microHTML = ejs.render(micro_template, {//renders the file
       filename: __dirname + '/views/micro.ejs',
-      data: object
+      data: JSON.parse(country_info),
+      data2: object
     });
   fs.writeFileSync('build/'+ object.entity +'.html', microHTML, 'utf8');
 }
+
+let about_html = ejs.render(about_template, {
+  filename: __dirname + '/views/about.ejs',
+  data: JSON.parse(country_info)
+});
+
+fs.writeFileSync('build/about.html', about_html, 'utf8');
