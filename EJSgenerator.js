1015d706ed536aa5ebefddf2bf6340f0cd221be1
erincorridon <erin.corridon22@trinityschoolnyc.org>
2021-12-08 09:44:59 -0500

    trying to fix

--- EJSgenerator.js
+++ EJSgenerator.js
@@ -5,6 +5,8 @@ let country_info = fs.readFileSync('data/plasticData.json', 'utf8');//read JSON 
 let index_template = fs.readFileSync('views/index.ejs', 'utf8');
 let micro_template = fs.readFileSync('views/micro.ejs', 'utf8');
 
+let about_template = fs.readFileSync('views/about.ejs', 'utf8');
+
 let data = JSON.parse(country_info);//convert JSON string into a JS object
 
 let index_html = ejs.render(index_template, {//renders the macropage
@@ -21,3 +23,10 @@ for(let object of data){//going through the values of data
     });
   fs.writeFileSync('build/'+ object.entity +'.html', microHTML, 'utf8');
 }
+
+let about_html = ejs.render(about_template, {
+  filename: __dirname + '/views/about.ejs',
+  data: JSON.parse(country_info)
+});
+
+fs.writeFileSync('build/about.html', about_html, 'utf8');
